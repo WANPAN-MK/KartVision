9d91abec88de128a52384fcd0ad365f2c3fd126f
わんぱん <zhiyingxiqi@gmail.com>
2024-09-21 23:45:19 +0900

    pathの変更

--- src/kartvision/imagelib.py
+++ src/kartvision/imagelib.py
@@ -9,20 +9,21 @@ def init():
     if not os.path.isdir(static_dir):
         os.mkdir(static_dir)
 
-    images_dir = "src/kartvision/static" + "/images"
+    images_dir = os.path.join(static_dir, "history")
     if not os.path.isdir(images_dir):
         os.mkdir(images_dir)
 
 # スクリーンショットを撮り保存
 def screenshot(): 
     now = datetime.datetime.now()
-    screenshot_filename = f"src/kartvision/static/images/screenshot_{now.strftime('%Y%m%d_%H%M%S')}.png"
+    screenshot_filename = f"src/kartvision/static/history/screenshot_{now.strftime('%Y%m%d_%H%M%S')}.png"
     screenshot = pyautogui.screenshot()
     screenshot.save(screenshot_filename)
     print(f"スクリーンショットを保存しました: {screenshot_filename}")
 
 def get_screenshot() -> List[str]:
-    directory = "src/kartvision/static/images"
-    images = ['images/' + f for f in os.listdir(directory) if f.endswith('.png')]
+    # ToDo 時間でソート
+    directory = "src/kartvision/static/history"
+    images = ['history/' + f for f in os.listdir(directory) if f.endswith('.png')]
     print(images)
     return images
