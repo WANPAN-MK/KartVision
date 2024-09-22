e38f81fbea0961a8bc48a6e54a6ea61873ad965d
わんぱん <zhiyingxiqi@gmail.com>
2024-09-22 23:20:06 +0900

    サーバーの起動とflag検出

--- src/kartvision/app.py
+++ src/kartvision/app.py
@@ -2,17 +2,16 @@ import sys
 from flask import render_template, Flask
 from google.cloud import vision
 import imagelib
+import threading
 
-# regions = [(1520, 204, 2125, 1596)]
-# client = vision.ImageAnnotatorClient()
+# 初期化
 imagelib.init()
-imagelib.screenshot()
 
 data = [
-    {'team':"A", 'points':65},
-    {'team':"B", 'points':56},
-    {'team':"C", 'points':31},
-    {'team':"D", 'points':12},
+    {'team': "A", 'points': 65},
+    {'team': "B", 'points': 56},
+    {'team': "C", 'points': 31},
+    {'team': "D", 'points': 12},
 ]
 
 app = Flask(__name__)
@@ -26,3 +25,14 @@ def history():
     images = imagelib.get_screenshot()
     return render_template("history.html", images=images)
 
+def start_flag_detection():
+    imagelib.take_screenshot_when_flag_detected()
+
+if __name__ == "__main__":
+    # 画像処理スレッドを開始
+    flag_detection_thread = threading.Thread(target=start_flag_detection)
+    flag_detection_thread.daemon = True  # メインプログラム終了時にスレッドも終了
+    flag_detection_thread.start()
+
+    # Flaskアプリを実行
+    app.run()
--- src/kartvision/imagelib.py
+++ src/kartvision/imagelib.py
@@ -1,5 +1,6 @@
 import pyautogui
 import datetime
+from time import sleep
 import os
 from typing import List
 
@@ -27,3 +28,50 @@ def get_screenshot() -> List[str]:
     images = ['history/' + f for f in os.listdir(directory) if f.endswith('.png')]
     print(images)
     return images
+
+
+def take_screenshot_when_flag_detected():
+    # 設定
+    wait_time_before_screenshot = 0.3
+    flag_image = "src/kartvision/static/images/flag_trigger.png"
+    
+    running = True
+    while running:
+        sleep(0.1)
+        print("待機中...")
+        try:
+            location = pyautogui.locateOnScreen(flag_image, confidence=0.7)
+        except pyautogui.ImageNotFoundException:
+            continue
+            
+        if location:
+            print("日本国旗が見つかりました。スクリーンショットを撮る前に待機します...")
+            sleep(wait_time_before_screenshot)
+            screenshot()
+            running = False
+            
+        #
+        #         regions = [
+        #             (1520, 204, 2125, 309), (1520, 321, 2125, 426), 
+        #             (1520, 438, 2125, 543), (1520, 555, 2125, 660), 
+        #             (1520, 672, 2125, 777), (1520, 789, 2125, 894), 
+        #             (1520, 906, 2125, 1011), (1520, 1023, 2125, 1128),
+        #             (1520, 1140, 2125, 1245), (1520, 1257, 2125, 1362),
+        #             (1520, 1374, 2125, 1479), (1520, 1491, 2125, 1596)
+        #         ]
+
+        #         text = crop_and_combine_image(screenshot_filename, regions)
+        #         print(text)
+        #         team_scores = aggregate_scores(text)
+        #         print_team_scores(team_scores)
+        #         adjust_score(team_scores)
+        #         generate_html()
+        #         print("待機中...")
+        #         time.sleep(wait_time_after_screenshot)
+
+        # except pyautogui.ImageNotFoundException:
+        #     print("読み込み中... ")
+        # except Exception as e:
+        #     print(f"エラーが発生しました: {e}")
+
+        # time.sleep(0.1)
