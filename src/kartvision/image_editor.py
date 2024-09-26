2530b9b253379e7b7caaa099ed2cd41beb05585a
わんぱん <zhiyingxiqi@gmail.com>
2024-09-27 00:25:11 +0900

    必要のない関数の削除

--- src/kartvision/image_editor.py
+++ src/kartvision/image_editor.py
@@ -1,17 +1,5 @@
 from PIL import Image, ImageEnhance, ImageFilter
 
-def divide_rectangle_into_regions(x1, y1, x2, y2):
-    num_divisions = 12
-    total_height = y2 - y1
-    region_height = total_height / num_divisions
-    regions = []
-    for i in range(num_divisions):
-        y_start = y1 + region_height * i
-        y_end = y_start + region_height
-        region = (x1, int(y_start), x2, int(y_end))
-        regions.append(region)
-    return regions
-
 def preprocess_image():
     im = Image.open('src/kartvision/static/cashe/clip_screenshot.png') # ToDo Openしないやり方で
     im = im.convert('L') # 白黒変換
